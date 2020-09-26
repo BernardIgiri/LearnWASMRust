1617babf5b26ed24f8913be6462a414d08550f16
Bernard Igiri <bernard.igiri@gmail.com>
2020-09-26 19:26:25 -0400

    Changed Yew application to a Todo implementation.

--- yew-app/src/lib.rs
+++ yew-app/src/lib.rs
@@ -1,28 +1,81 @@
+#![recursion_limit = "1024"]
+
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
 struct Model {
     link: ComponentLink<Self>,
-    value: i64,
+    input: String,
+    edit_input: String,
+    todos: Vec<Todo>,
+}
+
+struct Todo {
+    text: String,
+    edit: bool,
 }
 
 enum Msg {
-    AddOne,
+    Add,
+    Update(String),
+    Remove(usize),
+    StartEditing(usize),
+    FinishEditing(usize),
+    UpdateEdit(String),
+    RemoveAll,
+    Nothing,
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
+
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             link,
-            value: 0,
+            todos: vec![],
+            input: "".to_string(),
+            edit_input: "".to_string(),
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1
+            Msg::Add => {
+                let t = Todo {
+                    text: self.input.clone(),
+                    edit: false,
+                };
+                self.todos.push(t);
+                self.input = "".to_string();
+            }
+            Msg::Update(s) => {
+                self.input = s;
+            }
+            Msg::Remove(i) => {
+                self.todos.remove(i);
+            }
+            Msg::RemoveAll => {
+                self.todos = vec![];
+            }
+            Msg::UpdateEdit(s) => {
+                self.edit_input = s;
+            }
+            Msg::StartEditing(i) => {
+                let todo = self.todos.get_mut(i).unwrap();
+                if !todo.edit {
+                    todo.edit = true;
+                    self.edit_input = todo.text.clone();
+                }
+            }
+            Msg::FinishEditing(i) => {
+                let todo = self.todos.get_mut(i).unwrap();
+                if todo.edit {
+                    todo.edit = false;
+                    todo.text = self.edit_input.clone();
+                }
+            }
+            Msg::Nothing => {}
         }
         true
     }
@@ -35,11 +88,54 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
+        let view_todo_edit = |(i, todo): (usize, &Todo)| {
+            if todo.edit {
+                html! {
+                    <label><input type="text"
+                        value=&self.edit_input,
+                        oninput=self.link.callback(|e: InputData| Msg::UpdateEdit(e.value)),
+                        onkeypress=self.link.callback(move |e: KeyboardEvent| {
+                            if e.key() == "Enter" {Msg::FinishEditing(i)} else {Msg::Nothing}
+                        }),
+                        />
+                    </label>
+                }
+            } else {
+                html! {
+                    <label > {format!("{} ", &todo.text)}
+                    </label>
+                }
+            }
+        };
+        let view_todo = |(i, todo): (usize, &Todo)| {
+            html! {
+                <li ondblclick=self.link.callback(move |_| Msg::StartEditing(i)),>
+                    { view_todo_edit((i, &todo))}
+                    <button title="Delete".to_string(), onclick=self.link.callback(move |_| Msg::Remove(i)),>{"X"}</button>
+                </li>
+            }
+        };
         html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+            <>
+                <div>
+                    <h1>{"My Todo App"}</h1>
+                    <input
+                        placeholder="what do you want to do?",
+                        value=&self.input,
+                        oninput=self.link.callback(|e: InputData| Msg::Update(e.value)),
+                        onkeypress=self.link.callback(|e: KeyboardEvent| {
+                            if e.key() == "Enter" {Msg::Add} else {Msg::Nothing}
+                        }),/>
+                </div>
+                <div>
+                    <button onclick=self.link.callback(|_| Msg::RemoveAll), >{"Delete all Todos!"}</button>
+                </div>
+                <div>
+                    <ul>
+                    {for self.todos.iter().enumerate().map(view_todo)}
+                    </ul>
+                </div>
+            </>
         }
     }
 }
